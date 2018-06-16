63b01e9f80c18677fed6a8e64aa2f49d8e8ba7bc
Abegod <dr.abegod@gmail.com>
2018-06-16 22:38:55 +0200

    2

--- server.js
+++ server.js
@@ -15,12 +15,24 @@ const server = require('server');
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
+const mongoose = require ('mongoose');
+mongoose.connect('mongodb://abegod:ingeringer1@ds261470.mlab.com:61470/ingerdb');
+mongoose.connection.once('open', function() {
+    console.log('connection made, hurra hurra!');
+}).on('error', function(error){
+    console.log('Connection error', error);
+});
+const Schema = mongoose.Schema;
+const RaffleSchema = new Schema({
+    content: String
+});
+const RaffleTicket = mongoose.model('RaffleTicket', RaffleSchema);
+
 let prefix = "!";
 
 client.on("message", (message) => {
     const args = message.content.slice(prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
-    const pickArgs = args.join(" ").split(",");
 
     if (command === "pick") {
         const pickArgs = args.join(" ").split(",");
@@ -28,6 +40,14 @@ client.on("message", (message) => {
         const chosenArg = pickArgs[randomArg].trim();
         const replies = 5;
         const chosenReply = Math.floor(Math.random() * replies) + 1;
+
+        if (pickArgs == 0) {
+            message.reply('Hvad er det jeg skal jeg vælge..?');
+            return;
+        } else if(pickArgs == 1) {
+            message.reply('Du har kun givet mig én valgmulighed. Adskil dem med kommaer');
+            return;
+        }
         switch(chosenReply){
             case 1:
                 message.reply("Helt klart " + chosenArg);
@@ -46,48 +66,58 @@ client.on("message", (message) => {
                 break;
         }
     }
-
-    if (command === "yn" || command === "yesno" || command === "janej") {
-        const replies = 12;
-        const chosenReply = Math.floor(Math.random() * replies) + 1;
-        switch(chosenReply){
-            case 1:
-                message.reply("Aldrig i livet!");
-                break;
-            case 2:
-                message.reply("Fuck nej");
-                break;
-            case 3:
-                message.reply("Nej selvfølgelig ikke");
-                break;
-            case 4:
-                message.reply("ææhm nej");
-                break;
-            case 5:
-                message.reply("Hmm den er svær... men nej.");
-                break; 
-            case 6:
-                message.reply("Niksen biksen, Karen Blixen");
-                break;
-            case 7:
-                message.reply("Fuck ja!!");
-                break;
-            case 8:
-                message.reply("Ja, det ville jeg mene");
-                break;
-            case 9:
-                message.reply("stensikkert JA :)");
-                break;
-            case 10:
-                message.reply("Jeps");
-                break;
-            case 11:
-                message.reply("Tjooh... tjah... Ja!");
-                break; 
-            case 12:
-                message.reply("Hahaha ja :'D");
-                break;
+    else if (command === "yn" || command === "yesno" || command === "janej") {
+        var replies = [
+        'Aldrig i livet!',
+        'Fuck nej',
+        'Nej selvfølgelig ikke',
+        'ææhm nej',
+        'Hmm den er svær... men nej.',
+        'Niksen biksen, Karen Blixen',
+        'Fuck ja!!',
+        'Ja, det ville jeg mene',
+        'stensikkert JA :)',
+        'Jeps',
+        'Tjooh... tjah... Ja!',
+        'Haha ja'
+        ];
+        const chosenReply = Math.floor(Math.random() * replies.length);
+        message.reply(replies[chosenReply]);
+    }   
+    else if (command === 'tombola') {
+        if (args == 0) {
+            RaffleTicket.count().exec(function (err, count) {
+                var random = Math.floor(Math.random() * count)
+                RaffleTicket.findOne().skip(random).exec(
+                  function (err, result) {
+                    console.log(result);
+                    message.reply('Tillykke, du har vundet en ' + result.content + '!');
+                  })
+              })
+        } else {
+            var raffleArg = args.join(" ").trim();
+            var newTicket = new RaffleTicket({ content: raffleArg });
+            newTicket.save(function (err, newTicket) {
+                if (err) return console.error(err);
+                message.reply(raffleArg + ' er lagt som præmie i tombolaen.');
+            });
         }
+    }
+    else if(message.isMentioned(client.user)){
+        var replies = [
+            'er du dansker? For det er du bestemt ikke i mine øjne, din usle narkoman',
+            '2 sek, ruller lige',
+            'lol hvad? fuck jeg er basket..',
+            'dig og mig, my place, nu! og husk sagerne',
+            'tag mig din tyr, tag mig lige nu og her! please PLEASE!!!',
+            'SKRID',
+            'hey snuske, kom herhen til mor <3',
+            'ses vi til Sommersang I Mariehaven?',
+            'åårrh... åhhhh.. åh åh ÅÅH AAAH AAAARRRRRRHHHHHHHHHHH!! ... puha',
+            'flat earthere har faktisk en pointe'
+            ];
+            const chosenReply = Math.floor(Math.random() * replies.length);
+            message.reply(replies[chosenReply]);
     }
   });
 
